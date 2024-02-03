473d27926f9f59f9aaff1829d4e69766a9de8c83
lomtAnri <136019903+lomtAnri@users.noreply.github.com>
2024-02-03 23:47:26 +0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const btnEl = document.querySelector(".btn");
+
+btnEl.addEventListener("mouseover", (Event)=> {
+    const x = (Event.pageX - btnEl.offsetLeft);
+    const y = (Event.pageY - btnEl.offsetTop);
+
+    btnEl.style.setProperty("--xPos", x + "px");
+    btnEl.style.setProperty("--yPos", y + "px");
+});
+
